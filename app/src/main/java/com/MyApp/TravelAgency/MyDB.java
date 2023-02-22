6aad81a51a15671278dd417c6a73da502aed6b31
NikolaosProgios <nikosprogios@gmail.com>
2023-02-22 19:45:29 +0200

    Re-Upload

---
+++ app/src/main/java/com/MyApp/TravelAgency/MyDB.java
@@ -0,0 +1,9 @@
+
+package com.MyApp.TravelAgency;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AgencyTable.class, TripTable.class, TripPackageTable.class}, version = 1)
+public abstract class MyDB extends RoomDatabase{
+    public abstract MyDAO myDao();
+}
---
+++ app/src/main/java/com/MyApp/TravelAgency/NoficationActivity.java
@@ -0,0 +1,15 @@
+
+package com.MyApp.TravelAgency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoficationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nofication);
+    }
+}
